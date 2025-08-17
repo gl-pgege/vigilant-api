d52704c3e825257a4d01cc6dcfe6b36b092f5c42
Paul Gege <pgege@macbookpro.home>
2025-08-17 18:25:30 -0400

    test deploy

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Vigilant Flow API is running!';
+    return 'Vigilant Flow API is runninnnnnnggg!';
   }
 
   getHealth(): { status: string; timestamp: string } {
